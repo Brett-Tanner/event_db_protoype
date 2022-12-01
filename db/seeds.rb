fd343af5959c92ac2ba286ee48402343c9676abf
Brett Tanner <bretttanner171@gmail.com>
2022-12-02 08:27:38 +0900

    Seed Users, require sign-in for all actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,54 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+
+User.create([
+  {
+    name: "カレン福原",
+    phone: "07042159870",
+    role: 0,
+    email: "karen_supe@gmail.com",
+    password: "password",
+    password_confirmation: "password"
+  },
+  {
+    name: "結衣田中",
+    phone: "07042159871",
+    role: 0,
+    email: "random@gmail.com",
+    password: "password",
+    password_confirmation: "password"
+  },
+  {
+    name: "所平小谷",
+    phone: "07042159872",
+    role: 0,
+    email: "baseball@gmail.com",
+    password: "password",
+    password_confirmation: "password"
+  },
+  {
+    name: "ミノル",
+    phone: "07042159873",
+    role: 1,
+    email: "okurayama@gmail.com",
+    password: "password",
+    password_confirmation: "password"
+  },
+  {
+    name: "ひらがな",
+    phone: "07042159874",
+    role: 1,
+    email: "test@gmail.com",
+    password: "password",
+    password_confirmation: "password"
+  },
+  {
+    name: "admin",
+    phone: "07042159875",
+    role: 2,
+    email: "admin@gmail.com",
+    password: "adminpass",
+    password_confirmation: "adminpass"
+  }
+])
+
+puts "Created 3 parents, 2 SMs and an Admin"
