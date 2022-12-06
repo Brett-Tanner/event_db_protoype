96238662a20053c63e2400b1edfa3cdc7838d80d
Brett Tanner <bretttanner171@gmail.com>
2022-12-06 13:40:45 +0900

    Add links to check children

--- app/models/child.rb
+++ app/models/child.rb
@@ -4,5 +4,7 @@ class Child < ApplicationRecord
   has_many :registrations, dependent: :destroy
   accepts_nested_attributes_for :registrations
   has_many :event_days, through: :registrations
+  has_many :events, through: :event_days
   has_many :emergency_contacts, dependent: :destroy
+  has_one :school, through: :user
 end
