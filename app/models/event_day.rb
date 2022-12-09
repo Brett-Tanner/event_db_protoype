79586888378b3497227ed471c00d831aed6f9228
Brett Tanner <bretttanner171@gmail.com>
2022-12-09 13:30:40 +0900

    Correctly count children attending each day

--- app/models/event_day.rb
+++ app/models/event_day.rb
@@ -3,4 +3,8 @@ class EventDay < ApplicationRecord
 
   has_many :registrations, dependent: :destroy
   has_many :children, through: :registrations
+
+  def children_attending
+    self.registrations.where(attend_morning: true).or(self.registrations.where(attend_afternoon: true)).count
+  end
 end
